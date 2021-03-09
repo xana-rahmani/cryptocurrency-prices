15cecbb3467c8a19c2e3a13971ba22b7ced5e952
xana <xanarahmani@gmail.com>
2021-03-10 03:25:25 +0330

    Improve Coding.   v0.4

--- app/src/main/java/com/example/cryptho/MainModelView.java
+++ app/src/main/java/com/example/cryptho/MainModelView.java
@@ -1,8 +1,6 @@
 package com.example.cryptho;
 
-import android.annotation.SuppressLint;
 import android.os.Handler;
-import android.os.Message;
 import android.util.Log;
 
 import org.decimal4j.util.DoubleRounder;
@@ -12,6 +10,7 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -53,31 +52,68 @@ public class MainModelView {
         String ApiToken = "7fc06983-3d6c-437a-8bc5-09bd5b4d19bc";
         String ApiHeaderFormat = "X-CMC_PRO_API_KEY";
 
-        if (CoinMarketCapUrl == null) CreateCoinMarketCapUrlBuilder();
+        if (CoinMarketCapUrl == null) CreateCoinMarketCapUrl();
+        // start is offset (1-based index) of the paginated list of items to return.
+        int start = NumberOfCoins + 1;
+        String url = CoinMarketCapUrl + "&start=" + start;
 
         networkExecutorService.execute(new Runnable() {
             @Override
             public void run() {
 
                 HttpRequest httpRequest = new HttpRequest();
-                Response response = httpRequest.call(CoinMarketCapUrl, ApiToken, ApiHeaderFormat);
+                Response response = httpRequest.call(url, ApiToken, ApiHeaderFormat);
 
                 if (response == null) return;
 
                 try {
-                    Log.v("ans: ", response.body().string());
-                } catch (IOException e) {
+                    // convert string response to json object.
+                    JSONObject jsonObject = new JSONObject(Objects.requireNonNull(response.body()).string());
+                    AddNewCoinsData(jsonObject);
+                } catch (IOException | JSONException e) {
                     e.printStackTrace();
                 }
             }
         });
     }
 
-    private void CreateCoinMarketCapUrlBuilder(){
+    private void CreateCoinMarketCapUrl(){
         String UrlFormat = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest";
         HttpUrl.Builder urlBuilder = HttpUrl.parse(UrlFormat).newBuilder();
         urlBuilder.addQueryParameter("limit", "10");
         urlBuilder.addQueryParameter("convert", "USD");
         CoinMarketCapUrl = urlBuilder.build().toString();
+    }
+
+    private void AddNewCoinsData(JSONObject jsonCoinsData){
+        try {
+            JSONArray dataArray = jsonCoinsData.getJSONArray("data");
+
+            for (int i = 0; i < dataArray.length(); i++) {
+
+                JSONObject coinData = dataArray.getJSONObject(i);
+                String coin_name = coinData.getString("name");
+                String coin_symbol = coinData.getString("symbol");
+
+                JSONObject coinUsdValue = coinData.getJSONObject("quote").getJSONObject("USD");
+                double coin_price = coinUsdValue.getDouble("price");
+                double change_1h = DoubleRound(coinUsdValue.getDouble("percent_change_1h"));
+                double change_24h = DoubleRound(coinUsdValue.getDouble("percent_change_24h"));
+                double change_7d = DoubleRound(coinUsdValue.getDouble("percent_change_7d"));
+
+                Log.d("#Debug", coin_name + " " + coin_symbol);  //TODO: for test
+
+            }
+
+            //TODO: After add new coins >> update NumberOfCoins
+            NumberOfCoins += 10;
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private double DoubleRound(double d) {
+        return DoubleRounder.round(d, 2);
     }
 }
