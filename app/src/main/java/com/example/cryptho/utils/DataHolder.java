d4e34c4ed9bab81e1d022fc2d66cd8ce262d2c05
xana <xanarahmani@gmail.com>
2021-03-11 01:03:18 +0330

    Add RecyclerView (don't work)  ---

--- app/src/main/java/com/example/cryptho/utils/DataHolder.java
+++ app/src/main/java/com/example/cryptho/utils/DataHolder.java
@@ -3,7 +3,7 @@ package com.example.cryptho.utils;
 import java.util.ArrayList;
 
 public class DataHolder {
-    private final ArrayList<CoinData> CoinsData = new ArrayList();
+    public ArrayList<CoinData> CoinsData = new ArrayList();
 
     private static final DataHolder dataHolder = new DataHolder();
     public static synchronized DataHolder getInstance() {
@@ -21,7 +21,7 @@ public class DataHolder {
                                     double percent_change_1h,
                                     double percent_change_24h,
                                     double percent_change_7d) {
-        synchronized (this.CoinsData){
+        synchronized (this){
             for (CoinData cd : CoinsData){
                 if (cd.getName().equals(name)){
                     cd.setPrice(price);
@@ -35,67 +35,5 @@ public class DataHolder {
                     percent_change_24h,
                     percent_change_7d));
         }
-    }
-}
-
-
-class CoinData {
-    private final String name;
-    private final String symbol;
-    private double price;
-    private double percent_change_1h;
-    private double percent_change_24h;
-    private double percent_change_7d;
-
-    public CoinData(String name, String symbol, double price,
-                    double percent_change_1h,
-                    double percent_change_24h,
-                    double percent_change_7d) {
-        this.name = name;
-        this.symbol = symbol;
-        this.price = price;
-        this.percent_change_1h = percent_change_1h;
-        this.percent_change_24h = percent_change_24h;
-        this.percent_change_7d = percent_change_7d;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public void setPercent_change_1h(double percent_change_1h) {
-        this.percent_change_1h = percent_change_1h;
-    }
-
-    public void setPercent_change_24h(double percent_change_24h) {
-        this.percent_change_24h = percent_change_24h;
-    }
-
-    public void setPercent_change_7d(double percent_change_7d) {
-        this.percent_change_7d = percent_change_7d;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getSymbol() {
-        return symbol;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public double getPercent_change_1h() {
-        return percent_change_1h;
-    }
-
-    public double getPercent_change_24h() {
-        return percent_change_24h;
-    }
-
-    public double getPercent_change_7d() {
-        return percent_change_7d;
     }
 }
